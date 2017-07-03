540c1fcaeb0ab8edf987f182511326fa3b358420
oumar <oumar.youssouf0605@gmail.com>
2017-07-03 17:54:28 +0200

    Add while

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ function game() {
   var Ordi = 0;
 
 // var a=prompt("Jouer");
+while (joueur < 3 && Ordi < 3 ){
 var a=prompt("Jouer");
 var b = Math.floor((Math.random() * 3) + 1);
+
 
 
 if(b==1){
@@ -23,27 +25,27 @@ if(a==b){
 }
 else if(a==="pierre" && b==="papier"){
   alert("l'ordi gagne. L'ordi avait choisit : " + b);
-
+Ordi++;
 }
 else if(a==="ciseaux" && b==="papier"){
   alert("Tu gagne. L'ordi avait choisit : " + b);
-
+joueur++;
 }
 else if(a==="papier" && b==="pierre"){
   alert("Tu gagne. L'ordi avait choisit : " + b);
-
+joueur++;
 }
 else if(a==="papier" && b==="ciseaux"){
   alert("L'ordi gagne. L'ordi avait choisit : " + b);
-
+Ordi++;
 }
 else if(a==="ciseaux" && b==="pierre"){
   alert("L'ordi gagne. L'ordi avait choisit : " + b);
-
+Ordi++;
 }
 else if(a==="ciseaux" && b==="papier"){
   alert("Tu gagne. L'ordi avait choisit : " + b);
-
+joueur++;
 }
 else if (a === "" || a !== "pierre" || a !== "papier" || a !== "ciseaux") {
 alert("Entrez soit pierre, papier ou ciseaux");
@@ -55,4 +57,5 @@ alert("Entrez soit pierre, papier ou ciseaux");
 // alert("Tu as perdu la partie !");
 // }
 // document.getElementById('demo').innerHTML = "Ordi " + "" + b;
+}
 }
